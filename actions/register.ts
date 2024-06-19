8f85f2d1b503c8a92cb7bb66aaa1c3b917755b48
Irakli Maisuradze <irakli.maisuradze31.19@gmail.com>
2024-06-19 15:01:24 +0400

    added resend email verification function updated db shcema

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,8 @@ import * as z from "zod";
 import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validateFields = registerSchema.safeParse(values);
@@ -30,6 +32,9 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
   });
 
   // Sent verify email token
+  const verificationToken = await generateVerificationToken(email);
 
-  return { success: "User Created!" };
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+  return { success: "Confirmation email sent!" };
 };
